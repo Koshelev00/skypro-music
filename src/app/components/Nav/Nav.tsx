18b28eb12f74e4b778ad6af4822f9a9e0fe28c09
Alexey Koshelev <koschelev00@mail.ru>
2025-06-10 18:12:05 +0300

    1

---
+++ src/app/components/Nav/Nav.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import styles from './nav.module.css';
+import Link from 'next/link';
+export default function Nav() {
+    return (
+          <nav className={styles.main__nav}>
+            <div className={styles.nav__logo}>
+              {/*TODO: img –> Image*/}
+              <Image
+                width={250}
+                height={170}
+                className={styles.logo__image}
+                src="/logo.png"
+                alt={'logo'}
+              />
+            </div>
+            <div className={styles.nav__burger}>
+              <span className={styles.burger__line}></span>
+              <span className={styles.burger__line}></span>
+              <span className={styles.burger__line}></span>
+            </div>
+            <div className={styles.nav__menu}>
+              <ul className={styles.menu__list}>
+                <li className={styles.menu__item}>
+                  {/*TODO: a -> Link*/}
+                  <Link href="#" className={styles.menu__link}>
+                    Главное
+                  </Link>
+                </li>
+                <li className={styles.menu__item}>
+                  <Link href="#" className={styles.menu__link}>
+                    Мой плейлист
+                  </Link>
+                </li>
+                <li className={styles.menu__item}>
+                  <Link href="../signin.html" className={styles.menu__link}>
+                    Выйти
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    )
+}
