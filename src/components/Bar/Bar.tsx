697fc316722d12356c3f62ecb8241b46bd223a3f
Koshelev <koschelev00@mail.ru>
2025-08-12 23:06:00 +0300

    1

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -33,11 +33,14 @@ export default function Bar() {
 
   useEffect(() => {
     setIsLoadedTrack(false);
+    
   }, [currentTrack]);
   useEffect(() => {
     const audio = audioRef.current;
+    
 
     if (!audio || !currentTrack) return;
+    
 
     if (isPlay) {
       audio.play().catch((err) => console.warn('Autoplay error:', err));
@@ -45,6 +48,7 @@ export default function Bar() {
       audio.pause();
     }
   }, [isPlay, currentTrack]);
+  
 
   const playTrack = useCallback(() => {
     if (audioRef.current && currentTrack) {
@@ -106,6 +110,7 @@ export default function Bar() {
       dispatch(setNextTrack());
     }
   }, [dispatch]);
+   if (!currentTrack) return <></>;
 
   return (
     <div className={styles.bar}>
